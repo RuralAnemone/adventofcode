6e953cf471f0b6c329ad23130ccbf5b817c162b7
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-12-01 17:35:37 -0600

    going back to rpelit lol

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
-const elf = fs.readFileSync("./elf", "utf-8")
+const elf = fs.readFileSync("./elf.txt", "utf-8")
 console.time("time")
 var arr = []
 var max = 0;
+
+function bblSort(arr){
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < arr.length-i-1; j++){
+      if (arr[j] > arr[j+1]) {  
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr
+}
 
 elf.split("\n\n").forEach(e => {
   let sum = 0;
@@ -12,16 +25,12 @@ elf.split("\n\n").forEach(e => {
   arr.push(sum);
 })
 
+console.log(elf.includes("\n\n"))
+
 // elf.split("\n\n").map()
 
-arr.forEach(e => {
-  // sort
-  
-  // not sure why I just looked up "js bubble sort" uh
-  // well anyways
-  // this is actually my own thoughts though don't worry :)
-  
-})
+arr = bblSort(arr);
 
-console.log(max)
+console.log("max:",arr[0])
+
 console.timeEnd("time")
